8466f007fb2c9458f2b7f2d6a9f28af3f86e6a1b
caadiq <caadiq@gmail.com>
2024-01-15 19:22:06 +0900

    프로젝트 생성

---
+++ src/main/java/com/credential/cubrism/server/CubrismApplication.java
@@ -0,0 +1,13 @@
+package com.credential.cubrism.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubrismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubrismApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/credential/cubrism/server/CubrismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credential.cubrism.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubrismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
