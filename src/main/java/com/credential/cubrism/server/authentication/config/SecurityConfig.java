412986c07e2b2a6b19ece15e2715a254ce79700f
seungsang <rovin000@hansung.ac.kr>
2024-02-09 21:49:54 +0900

    jwt
    jwt 완성

--- src/main/java/com/credential/cubrism/server/authentication/config/SecurityConfig.java
+++ src/main/java/com/credential/cubrism/server/authentication/config/SecurityConfig.java
@@ -1,29 +1,41 @@
 package com.credential.cubrism.server.authentication.config;
 
+import com.credential.cubrism.server.authentication.Jwt.JwtAuthenticationFilter;
+import com.credential.cubrism.server.authentication.Jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer -> { // 세션 사용 안함(jwt 사용시 세션 사용 안함)
+                    sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/error").permitAll()
-                        .requestMatchers("/auth/signup/**").permitAll()
+                        .requestMatchers("/auth/signup").permitAll()
+                        .requestMatchers("/auth/signin").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
---
+++ src/main/java/com/credential/cubrism/server/authentication/dto/SignInDto.java
@@ -0,0 +1,15 @@
+package com.credential.cubrism.server.authentication.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class SignInDto {
+    private String email;
+    private String password;
+}
