68a5e23ef016b3aacf5e32acfd4f1b88ef56c7af
seungsang <rovin000@hansung.ac.kr>
2024-02-13 17:11:32 +0900

    jwt 갈아엎음(잘 동작함)
    구글 로그인 추가하는데 계속 오류가 나서, jwt 다른 방식으로 갈아엎었어

---
+++ src/main/java/com/credential/cubrism/server/Jwt/JwtLoginApiController.java
@@ -0,0 +1,48 @@
+package com.credential.cubrism.server.Jwt;
+
+import com.credential.cubrism.server.authentication.model.Users;
+import com.credential.cubrism.server.authentication.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/jwt-login")
+public class JwtLoginApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest loginRequest) {
+
+        Users user = userService.login(loginRequest);
+
+        // 로그인 아이디나 비밀번호가 틀린 경우 global error return
+        if(user == null) {
+            return"로그인 아이디 또는 비밀번호가 틀렸습니다.";
+        }
+
+        // 로그인 성공 => Jwt Token 발급
+
+        String secretKey = "my-secret-key-123123";
+        long expireTimeMs = 1000 * 60 * 60;     // Token 유효 시간 = 60분
+
+        String jwtToken = JwtTokenUtil.createToken(user.getEmail(), secretKey, expireTimeMs);
+
+        return jwtToken;
+    }
+
+    @GetMapping("/info")
+    public String userInfo(Authentication auth) {
+        Users loginUser = userService.getLoginUserByLoginId(auth.getName());
+
+        return String.format("loginId : %s\nnickname : %s\nrole : %s",
+                loginUser.getEmail(), loginUser.getNickname(), "USER");
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "관리자 페이지 접근 성공";
+    }
+}
--- src/main/java/com/credential/cubrism/server/authentication/service/UserService.java
+++ src/main/java/com/credential/cubrism/server/authentication/service/UserService.java
@@ -1,5 +1,6 @@
 package com.credential.cubrism.server.authentication.service;
 
+import com.credential.cubrism.server.Jwt.LoginRequest;
 import com.credential.cubrism.server.authentication.dto.UsersDTO;
 import com.credential.cubrism.server.authentication.model.Users;
 import com.credential.cubrism.server.authentication.repository.UserRepository;
@@ -8,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -34,5 +37,58 @@ public class UserService {
         users.setPassword(bCryptPasswordEncoder.encode(usersDto.getPassword()));
         users.setNickname(usersDto.getNickname());
         userRepository.save(users);
+    }
+
+    /**
+     *  로그인 기능
+     *  화면에서 LoginRequest(loginId, password)을 입력받아 loginId와 password가 일치하면 User return
+     *  loginId가 존재하지 않거나 password가 일치하지 않으면 null return
+     */
+    public Users login(LoginRequest req) {
+        Optional<Users> optionalUser = userRepository.findByEmail(req.getEmail());
+
+        // loginId와 일치하는 User가 없으면 null return
+        if(optionalUser.isEmpty()) {
+            return null;
+        }
+
+        Users user = optionalUser.get();
+
+        // 찾아온 User의 password와 입력된 password가 다르면 null return
+        if(!bCryptPasswordEncoder.matches(req.getPassword(), user.getPassword())) {
+            return null;
+        }
+
+        return user;
+    }
+
+    /**
+     * userId(Long)를 입력받아 User을 return 해주는 기능
+     * 인증, 인가 시 사용
+     * userId가 null이거나(로그인 X) userId로 찾아온 User가 없으면 null return
+     * userId로 찾아온 User가 존재하면 User return
+     */
+    public Users getLoginUserById(String email) {
+        if(email == null) return null;
+
+        Optional<Users> optionalUser = userRepository.findByEmail(email);
+        if(optionalUser.isEmpty()) return null;
+
+        return optionalUser.get();
+    }
+
+    /**
+     * loginId(String)를 입력받아 User을 return 해주는 기능
+     * 인증, 인가 시 사용
+     * loginId가 null이거나(로그인 X) userId로 찾아온 User가 없으면 null return
+     * loginId로 찾아온 User가 존재하면 User return
+     */
+    public Users getLoginUserByLoginId(String email) {
+        if(email == null) return null;
+
+        Optional<Users> optionalUser = userRepository.findByEmail(email);
+        if(optionalUser.isEmpty()) return null;
+
+        return optionalUser.get();
     }
 }
