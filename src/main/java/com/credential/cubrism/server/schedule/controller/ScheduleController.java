20d23f7bc3e1fe24b9bd6da9c69a38661801d689
caadiq <caadiq@gmail.com>
2024-03-02 18:02:55 +0900

    일정 목록 반환

--- src/main/java/com/credential/cubrism/server/schedule/controller/ScheduleController.java
+++ src/main/java/com/credential/cubrism/server/schedule/controller/ScheduleController.java
@@ -1,15 +1,13 @@
 package com.credential.cubrism.server.schedule.controller;
 
-import com.credential.cubrism.server.schedule.dto.AddScheduleRequestDTO;
+import com.credential.cubrism.server.common.dto.ErrorDTO;
+import com.credential.cubrism.server.schedule.dto.ScheduleDTO;
 import com.credential.cubrism.server.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,10 +16,50 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping("/add")
-    public ResponseEntity<?> addSchedule(@RequestBody AddScheduleRequestDTO addScheduleRequestDTO, Authentication authentication) {
+    public ResponseEntity<?> addSchedule(@RequestBody ScheduleDTO scheduleDTO, Authentication authentication) {
         try {
-            scheduleService.addSchedule(addScheduleRequestDTO, authentication);
+            scheduleService.addSchedule(scheduleDTO, authentication);
             return ResponseEntity.ok("일정 추가 성공");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+//    @PostMapping("/delete")
+//    public ResponseEntity<?> deleteSchedule(@RequestBody RemoveScheduleRequestDTO removeScheduleRequestDTO, Authentication authentication) {
+//        try {
+//            scheduleService.deleteSchedule(removeScheduleRequestDTO, authentication);
+//            return ResponseEntity.ok("일정 삭제 성공");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//        }
+//    }
+
+//    @PostMapping("/update")
+//    public ResponseEntity<?> updateSchedule(@RequestBody UpdateScheduleRequestDTO updateScheduleRequestDTO, Authentication authentication) {
+//        try {
+//            scheduleService.updateSchedule(updateScheduleRequestDTO, authentication);
+//            return ResponseEntity.ok("일정 수정 성공");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//        }
+//    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> getScheduleList(
+            @RequestParam(required = false) Integer year,
+            @RequestParam(required = false) Integer month,
+            Authentication authentication
+    ) {
+        try {
+            if (year == null && month == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO("'year', 'month' 파라미터가 필요합니다."));
+            } else if (year == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO("'year' 파라미터가 필요합니다."));
+            } else if (month == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO("'month' 파라미터가 필요합니다."));
+            }
+            return ResponseEntity.ok(scheduleService.getScheduleList(year, month, authentication));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
--- src/main/java/com/credential/cubrism/server/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/credential/cubrism/server/schedule/repository/ScheduleRepository.java
@@ -2,9 +2,13 @@ package com.credential.cubrism.server.schedule.repository;
 
 import com.credential.cubrism.server.schedule.model.Schedules;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ScheduleRepository extends JpaRepository<Schedules, UUID> {
-
+    @Query("SELECT s FROM Schedules s WHERE s.user.uuid = :userId AND YEAR(s.startDate) = :year AND MONTH(s.startDate) = :month ORDER BY s.startDate ASC")
+    List<Schedules> getScheduleByYearAndMonth(@Param("userId") UUID userId, @Param("year") int year, @Param("month") int month);
 }
