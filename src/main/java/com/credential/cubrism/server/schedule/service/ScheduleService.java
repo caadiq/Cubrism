7627a65c3e8c6e2a6b29dca9a4ff7df22e092b23
caadiq <caadiq@gmail.com>
2024-03-02 12:38:37 +0900

    중복 코드 제거하기 위해 메서드 분리

--- src/main/java/com/credential/cubrism/server/schedule/service/ScheduleService.java
+++ src/main/java/com/credential/cubrism/server/schedule/service/ScheduleService.java
@@ -1,15 +1,14 @@
 package com.credential.cubrism.server.schedule.service;
 
 import com.credential.cubrism.server.authentication.model.Users;
-import com.credential.cubrism.server.authentication.oauth.PrincipalDetails;
 import com.credential.cubrism.server.authentication.repository.UserRepository;
+import com.credential.cubrism.server.authentication.utils.AuthenticationUtil;
 import com.credential.cubrism.server.schedule.dto.AddScheduleRequestDTO;
 import com.credential.cubrism.server.schedule.model.Schedules;
 import com.credential.cubrism.server.schedule.repository.ScheduleRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -23,16 +22,7 @@ public class ScheduleService {
 
     @Transactional
     public void addSchedule(AddScheduleRequestDTO addScheduleRequestDTO, Authentication authentication) {
-        Object principal = authentication.getPrincipal();
-        Users user;
-        if (principal instanceof PrincipalDetails principalDetails) {
-            user = principalDetails.user();
-        } else if (principal instanceof String email) {
-            user = userRepository.findByEmail(email)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found with email : " + principal));
-        } else {
-            throw new IllegalArgumentException("Unsupported principal type: " + principal.getClass().getName());
-        }
+        Users user = AuthenticationUtil.getUserFromAuthentication(authentication, userRepository);
 
         Schedules schedules = new Schedules();
         schedules.setUser(user);
