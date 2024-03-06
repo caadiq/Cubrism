f181339767b56b7812b9e9b0455b5dee2b54aed0
caadiq <caadiq@gmail.com>
2024-03-06 10:52:21 +0900

    게시글 수정 기능 추가

---
+++ src/main/java/com/credential/cubrism/server/posts/dto/PostRegisterPostDTO.java
@@ -0,0 +1,10 @@
+package com.credential.cubrism.server.posts.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostRegisterPostDTO {
+    private String boardName;
+    private String title;
+    private String content;
+}
--- src/main/java/com/credential/cubrism/server/posts/dto/PostUpdatePostDTO.java
+++ src/main/java/com/credential/cubrism/server/posts/dto/PostUpdatePostDTO.java
@@ -3,7 +3,8 @@ package com.credential.cubrism.server.posts.dto;
 import lombok.Getter;
 
 @Getter
-public class RegisterPostRequestDTO {
+public class PostUpdatePostDTO {
+    private Long postId;
     private String boardName;
     private String title;
     private String content;
