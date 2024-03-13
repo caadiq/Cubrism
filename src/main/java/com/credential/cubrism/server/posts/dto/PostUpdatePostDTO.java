9e9f4a3ac5b8ae7b66f16114278c95b7feb9f5b3
caadiq <caadiq@gmail.com>
2024-03-13 09:01:35 +0900

    게시글 이미지 수정기능 추가

--- src/main/java/com/credential/cubrism/server/posts/dto/PostUpdatePostDTO.java
+++ src/main/java/com/credential/cubrism/server/posts/dto/PostUpdatePostDTO.java
@@ -1,6 +1,9 @@
 package com.credential.cubrism.server.posts.dto;
 
 import lombok.Getter;
+import org.springframework.lang.Nullable;
+
+import java.util.List;
 
 @Getter
 public class PostUpdatePostDTO {
@@ -8,4 +11,17 @@ public class PostUpdatePostDTO {
     private String boardName;
     private String title;
     private String content;
+    private List<Images> images;
+    @Nullable
+    private List<RemovedImages> removedImages;
+
+    @Getter
+    public static class Images {
+        private String imageUrl;
+    }
+
+    @Getter
+    public static class RemovedImages {
+        private String imageUrl;
+    }
 }
