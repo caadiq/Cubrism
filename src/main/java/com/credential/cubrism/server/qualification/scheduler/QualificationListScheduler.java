0b57fc19c68c63127270aeda4ba236a486aac557
caadiq <caadiq@gmail.com>
2024-03-14 11:32:33 +0900

    각 자격증을 따로 저장하는 방식으로 변경

--- src/main/java/com/credential/cubrism/server/qualification/scheduler/QualificationListScheduler.java
+++ src/main/java/com/credential/cubrism/server/qualification/scheduler/QualificationListScheduler.java
@@ -1,18 +1,14 @@
 package com.credential.cubrism.server.qualification.scheduler;
 
 import com.credential.cubrism.server.qualification.service.QualificationListService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class QualificationListScheduler {
     private final QualificationListService qualificationListService;
-
-    @Autowired
-    public QualificationListScheduler(QualificationListService qualificationListService) {
-        this.qualificationListService = qualificationListService;
-    }
 
     @Scheduled(cron = "0 0 0 * * *")
     public void saveQualificationListData() {
