65eb489554b53de82bdf3b167fbf129ba238b478
caadiq <caadiq@gmail.com>
2024-03-17 12:47:42 +0900

    카테고리에 ElasticSearch대신 캐싱을 사용하도록 변경

--- src/main/java/com/credential/cubrism/server/posts/dto/CategoryListGetDTO.java
+++ src/main/java/com/credential/cubrism/server/posts/dto/CategoryListGetDTO.java
@@ -3,9 +3,11 @@ package com.credential.cubrism.server.posts.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
 @AllArgsConstructor
-public class CategoryGetDTO {
+public class CategoryListGetDTO implements Serializable {
     private String code;
     private String category;
 }
--- src/main/java/com/credential/cubrism/server/posts/repository/ElasticSearchCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.credential.cubrism.server.posts.repository;
-
-import com.credential.cubrism.server.posts.entity.ElasticSearchCategory;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ElasticSearchCategoryRepository extends ElasticsearchRepository<ElasticSearchCategory, String> {
-
-}
