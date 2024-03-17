ca3f7a7c71fd6952f3f6be86862a1fad0d2fa720
seungsang <rovin000@hansung.ac.kr>
2024-03-17 20:22:02 +0900

    스터디 탈퇴

--- src/main/java/com/credential/cubrism/server/studygroup/repository/GroupMembersRepository.java
+++ src/main/java/com/credential/cubrism/server/studygroup/repository/GroupMembersRepository.java
@@ -1,12 +1,16 @@
 package com.credential.cubrism.server.studygroup.repository;
 
+import com.credential.cubrism.server.authentication.model.Users;
 import com.credential.cubrism.server.studygroup.entity.GroupMembers;
+import com.credential.cubrism.server.studygroup.entity.StudyGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface GroupMembersRepository extends JpaRepository<GroupMembers, UUID> {
 
+    Optional<GroupMembers> findByUserAndStudyGroup(Users user, StudyGroup studyGroup);
 }
