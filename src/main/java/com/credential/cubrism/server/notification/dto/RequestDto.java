ffc876e3967a6a0a03ce62326327ea592082dfc3
seungsang <rovin000@hansung.ac.kr>
2024-03-26 02:27:54 +0900

    알림 추가

---
+++ src/main/java/com/credential/cubrism/server/notification/dto/RequestDto.java
@@ -0,0 +1,12 @@
+package com.credential.cubrism.server.notification.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestDto {
+    private String targetToken;
+    private String title;
+    private String body;
+}
