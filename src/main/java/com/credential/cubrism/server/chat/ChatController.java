c9379770c7cab435dcd33a70967ea52bac1572b9
caadiq <caadiq@gmail.com>
2024-04-28 13:57:51 +0900

    패키지명 chatting -> chat 변경

---
+++ src/main/java/com/credential/cubrism/server/chat/ChatController.java
@@ -0,0 +1,12 @@
+package com.credential.cubrism.server.chat;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+
+
+}
