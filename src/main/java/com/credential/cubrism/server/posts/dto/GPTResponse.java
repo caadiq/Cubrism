efabf663dfc9c2886c7f96795381ae90cef1e37b
seungsang <rovin000@hansung.ac.kr>
2024-05-18 16:10:07 +0900

    게시글 등록,수정 시 ai답변 추가

---
+++ src/main/java/com/credential/cubrism/server/posts/dto/GPTResponse.java
@@ -0,0 +1,25 @@
+package com.credential.cubrism.server.posts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GPTResponse {
+
+    private List<Choice> choices;
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Choice {
+        private int index;
+        private Message message;
+
+    }
+}
